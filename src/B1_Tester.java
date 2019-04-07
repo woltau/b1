3839633c40dc170c9dbebb32b14e3dfcc59e7b9e
Wolfgang Taube <taube@hs-furtwangen.de>
2019-04-07 11:45:16 +0200

    Kommentar in Tester eingebaut

--- src/B1_Tester.java
+++ src/B1_Tester.java
@@ -8,6 +8,7 @@ public class B1_Tester {
 		B1_Berechne b2 = new B1_Berechne(result, 'B');
 		b1.start();
 		b2.start();
+		// zur Demonstration von wait()/notify()-Problemen
 		Thread.sleep(2000);
 		System.out.println(result.getResult());
 	}
